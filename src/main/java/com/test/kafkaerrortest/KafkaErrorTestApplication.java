e92118291d3fb022f6944cab482760f39c63f718
twoweekhee <myjjoo4758@icloud.com>
2024-08-20 09:33:07 +0900

    Initial commit

---
+++ src/main/java/com/test/kafkaerrortest/KafkaErrorTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.kafkaerrortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaErrorTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaErrorTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/kafkaerrortest/KafkaErrorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kafkaerrortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaErrorTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
