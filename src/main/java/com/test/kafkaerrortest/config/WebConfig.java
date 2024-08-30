3dd51b49bd163707d3b62848bb555ebdf630f57e
twoweekhee <myjjoo4758@icloud.com>
2024-08-30 17:57:03 +0900

    restTemplate config

---
+++ src/main/java/com/test/kafkaerrortest/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.test.kafkaerrortest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
+    }
+}
--- src/main/java/com/test/kafkaerrortest/consumer/KafkaConsumerService.java
+++ src/main/java/com/test/kafkaerrortest/consumer/KafkaConsumerService.java
@@ -21,8 +21,15 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
 
@@ -33,15 +40,70 @@ public class KafkaConsumerService {
 
 
     private final KafkaTemplate<String, MessageDto> kafkaTemplate;
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+    private static Process currentProcess;
+    private static final String JAR_PATH = "/Users/twoweekhee/Desktop/test/NHServer/build/libs/NHServer-0.0.1-SNAPSHOT.jar";
+
 
     public void listen(MessageDto messageDto) {
+//        if (!isProcessRunning(messageDto)) {
+//            ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", JAR_PATH);
+//            try {
+//                Process process = processBuilder.start();
+//                // 프로세스 출력 로깅
+//                new Thread(() -> {
+//                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+//                        String line;
+//                        while ((line = reader.readLine()) != null) {
+//                            log.info("JAR output: " + line);
+//                        }
+//                    } catch (IOException e) {
+//
+//                        log.error("Error reading JAR output", e);
+//                    }
+//                }).start();
+//
+//                // 프로세스가 준비될 때까지 대기 (예: 10초)
+//                Thread.sleep(10000);
+//            } catch (IOException e) {
+//                log.info("process cannot start");
+//                e.setStackTrace(e.getStackTrace());
+//            } catch (InterruptedException e) {
+//                log.error("Wait interrupted", e);
+//                Thread.currentThread().interrupt();
+//            }
+//        }
         URI uri = UriComponentsBuilder
                 .fromUriString("http://localhost")
                 .port(messageDto.getPort())
                 .path("/api/message")
                 .build().toUri();
+        log.info(messageDto.getMessage());
         ResponseEntity<String> result = restTemplate.postForEntity(uri, messageDto.getMessage(), String.class);
+    }
+
+    private static boolean isProcessRunning(MessageDto messageDto) {
+        if(currentProcess != null && currentProcess.isAlive()) {
+            HttpClient client = HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(1))
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:" + messageDto.getPort() + "/api"))
+                    .GET()
+                    .build();
+
+            try {
+                log.info("send data");
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                return response.statusCode() == 200;
+            } catch (ConnectException e) {
+                return false;
+            } catch (Exception e) {
+                log.warn("Error checking if port is in use", e);
+                return false;
+            }
+        }
+        return false;
     }
 
 
